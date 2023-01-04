cff55aba5b4bb35608ab51389632f5660ce9395b
Volkan Özbek <volkanozbk@hotmail.com>
2023-01-04 15:55:56 +0300

    update

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '',
+  rootNotionPageId: 'Siber-dev-d5e96b246d214759b8fb151dbba93af1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Volkan Özbek',
 
   // open graph metadata (optional)
-  description: 'Bir geliştiricinin notları',
+  description: 'Siber Dünyaya Hoş Geldin',
 
   // social usernames (optional)
   twitter: 'volkanozbektr',
